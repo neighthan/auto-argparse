3d0306d1946fd5b29798464d31866a4a7e85cb19
Nathan Hunt <neighthan.hunt@gmail.com>
2020-05-15 00:20:29 -0400

    Project skeleton.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="auto_argparse",
+    version="0.0.1",
+    url="https://github.com/neighthan/auto-argparse",
+    author="Nathan Hunt",
+    author_email="neighthan.hunt@gmail.com",
+    description="Automatically create argparse parsers.",
+    packages=find_packages(),
+)
